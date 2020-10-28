abc025e7a90003676a87908cf31f3f80ea3bb138
橘子味的猫° <2819836993@qq.com>
2020-10-28 20:35:47 +0800

    Login.vue 20% complete

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div style="display: flex;justify-content: center;margin-top: 150px">
+      <el-card style="width: 400px">
+        <div slot="header" class="clearfix">
+          <span>面包仓库管理系统</span>
+        </div>
+        <table align="center">
+          <tr>
+            <td align="left">用户名</td>
+            <td>
+              <el-input v-model="user.username" placeholder="请输入用户名"></el-input>
+            </td>
+          </tr>
+          <tr>
+            <td align="left">密码</td>
+            <td>
+              <el-input type="password" v-model="user.password" placeholder="请输入密码" @keydown.enter.native="doLogin"></el-input>
+              <!-- @keydown.enter.native="doLogin"当按下enter键的时候也会执行doLogin方法-->
+            </td>
+          </tr>
+          <tr>
+            <td colspan="2">
+              <!-- 点击事件的两种不同的写法v-on:click和 @click-->
+              <el-button style="width: 300px" type="primary" @click="doLogin">登录</el-button>
+            </td>
+          </tr>
+        </table>
+      </el-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return{
+      user:{
+        username:'',
+        password:'',
+      }
+    }
+  },
+  methods:{
+    doLogin(){//一点击登录按钮，这个方法就会执行
+      alert(JSON.stringify(this.user))//可以直接把this.user对象传给后端进行校验用户名和密码
+    }
+  }
+}
+</script>
