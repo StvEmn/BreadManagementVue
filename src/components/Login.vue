ae0f0cc89eb4b47efaa5e7f30bb248f9ba86c3ff
橘子味的猫° <2819836993@qq.com>
2020-11-03 14:53:54 +0800

    登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div style="display: flex;justify-content: center;margin-top: 150px">
+    <div style="display: flex; justify-content: center; margin-top: 150px">
       <el-card style="width: 400px">
         <div slot="header" class="clearfix">
           <span>面包仓库管理系统</span>
@@ -9,20 +9,30 @@
           <tr>
             <td align="left">用户名</td>
             <td>
-              <el-input v-model="user.username" placeholder="请输入用户名"></el-input>
+              <el-input
+                v-model="user.username"
+                placeholder="请输入用户名"
+              ></el-input>
             </td>
           </tr>
           <tr>
             <td align="left">密码</td>
             <td>
-              <el-input type="password" v-model="user.password" placeholder="请输入密码" @keydown.enter.native="doLogin"></el-input>
+              <el-input
+                type="password"
+                v-model="user.password"
+                placeholder="请输入密码"
+                @keydown.enter.native="doLogin"
+              ></el-input>
               <!-- @keydown.enter.native="doLogin"当按下enter键的时候也会执行doLogin方法-->
             </td>
           </tr>
           <tr>
             <td colspan="2">
               <!-- 点击事件的两种不同的写法v-on:click和 @click-->
-              <el-button style="width: 300px" type="primary" @click="doLogin">登录</el-button>
+              <el-button style="width: 300px" type="primary" @click="doLogin"
+                >登录</el-button
+              >
             </td>
           </tr>
         </table>
@@ -30,20 +40,41 @@
     </div>
   </div>
 </template>
+<style>
+.clearfix {
+  font-size: 20px;
+  text-align: center;
+}
+</style>
 <script>
 export default {
+  name: "Login",
   data() {
-    return{
-      user:{
-        username:'',
-        password:'',
-      }
-    }
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
   },
-  methods:{
-    doLogin(){//一点击登录按钮，这个方法就会执行
-      alert(JSON.stringify(this.user))//可以直接把this.user对象传给后端进行校验用户名和密码
-    }
-  }
-}
+  methods: {
+    doLogin() {
+      //一点击登录按钮，这个方法就会执行
+      if (this.user.username == "" || this.user.password == "") {
+        alert("账号密码不能为空");
+      } else {
+        this.$axios({
+          url: "http://localhost/authentication/login",
+          method: "POST",
+          data: {
+            username: this.user.username,
+            password: this.user.password,
+          },
+        }).then((res) => {
+          console.log(res);
+        });
+      }
+    },
+  },
+};
 </script>
